dcb04c7518f5e90d6f9607203657e54a59a783ae
abolfazl0215 <83293551+abolfazl0215@users.noreply.github.com>
2026-02-08 18:13:35 +0400

    add new url to cors

--- index.js
+++ index.js
@@ -135,7 +135,11 @@ app.use(morgan("dev"));
 // تنظیمات CORS
 const corsOptions = {
   // origin: "http://localhost:3000", // آدرس فرانت‌اند شما
-  origin: ["http://localhost:3000", process.env.FRONT_DOMAIN],
+  origin: [
+    "http://localhost:3000",
+    process.env.FRONT_DOMAIN,
+    "https://ulink-client.vercel.app",
+  ],
   // origin: "https://pounes.ir", // آدرس فرانت‌اند شما
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true, // ارسال کوکی‌ها به ازای درخواست‌های Cross-Origin
